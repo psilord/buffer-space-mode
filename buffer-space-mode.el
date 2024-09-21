3cc3f10e23bb258a311d96c99ddac3fa0ef97252
Peter Keller <psilord@cs.wisc.edu>
2024-09-20 21:36:34 -0500

    Finished bsm-error code. You get a nice stack too.

--- buffer-space-mode.el
+++ buffer-space-mode.el
@@ -19,70 +19,139 @@
 
 
 ;;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; Utilities
+;;; The bsm-error handling system and associated utilities.
 ;;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+;; used with bsm-error
+(defun bsm-snapshot-stack ()
+  "Return a vector with index 0 being the inner most stack frame containing
+the call to this function."
+  (let* ((bt (vector))
+         (frame-num 0))
+    (mapbacktrace
+     (lambda (a b c d)
+       (setq bt (vconcat bt (list (list frame-num (list a b c d)))))
+       (incf frame-num)))
+    bt))
+
+;; used with bsm-error
+(defun bsm-find-frame-num (selector-func bt start-index)
+  "Return the evaled frame number that contains the func specified and
+matches the evald critiera. evaldp can be: :not-evald, which means to
+only search unevald frames, :evald, which means search only evald
+frames, and :all, which means search both kinds of frames. If the frame
+cannot bne found under the conditions specified, return nil."
+  (cl-loop
+   for fid from start-index below (length bt)
+   do (destructuring-bind (frame-num frame) (elt bt fid)
+        (destructuring-bind (evald func args flags) frame
+          (when (funcall selector-func evald func args flags)
+            (cl-return frame-num))))))
+
+(defun bsm-collect-frames (selector-func bt start-index)
+  (cl-loop
+   for fid from start-index below (length bt)
+   when (destructuring-bind (frame-num frame) (elt bt fid)
+          (destructuring-bind (evald func args flags) frame
+            (funcall selector-func evald func args flags)))
+   collect (elt bt fid)))
+
+;; used with bsm-error
 (defun bsm-get-bsm-caller-of (&optional caller-of)
   "Get the symbol of the function this function is called from. If caller-of
 is nil, then get the direct bsm-parent of this function. Otherwise get the
 caller of the named function passed into caller-of (which is assumed to be
 on the stack). This function looks explicitly for bsm- prefixed names."
-  (let* ((bt (vector)))
-    ;; get a snapshot of the backtrace when we entered this function.
-    (let ((frame-num 0))
-      (mapbacktrace (lambda (a b c d)
-                      (setq bt
-                            (vconcat bt
-                                     (list (list frame-num (list a b c d)))))
-                      (incf frame-num))))
+  (let* ((bt (bsm-snapshot-stack))
+         (target-frame-num nil)
+         (caller-of (if caller-of caller-of 'bsm-get-bsm-caller-of))
+         (bsm-func-stack nil))
 
-    (princ (format ";; Full backtrace has %s frame(s):\n" (length bt)))
-    (princ ";; -------------------\n")
-    (dotimes (fid (length bt))
-      (let* ((frame-spec (elt bt fid))
-             (frame-num (first frame-spec))
-             (frame (second frame-spec)))
-        (let ((kind (eql't (car frame))))
-          (princ (format ";; [%3s] %s--> (%s %s ...)\n"
-                         frame-num
-                         (if kind
-                             "*"
-                           " |")
-                         (first frame)
-                         (second frame))))))
-    (princ ";; -------------------\n")
-
-    ;; Ok, now to do real work:
-
-    ;; 0. Starting at the bottom (most recent) frame.
-    ;;
-    ;; 1. Skip frames up to (t bsm-get-current-func-name)
-    ;;
-    ;; 2. if caller-of is nil, then skip upto the next t bsm- frame and
-    ;;    that's the answer.
-    ;;    if caller-of is non-nil, then skip up to the frame identified, then
-    ;;    skip upto the next t bsm- frame and that's the answer.
+    ;; Debugging, enable to t when you want to see more detail.
     (when nil
+      (princ (format ";; Full backtrace has %s frame(s):\n" (length bt)))
+      (princ ";; -------------------\n")
       (dotimes (fid (length bt))
         (let* ((frame-spec (elt bt fid))
                (frame-num (first frame-spec))
                (frame (second frame-spec)))
           (let ((kind (eql't (car frame))))
-            (princ (format ";; [%03s] %s--> (%s %s ...)\n"
-                           frame-num
-                           (if kind
-                               "*"
-                             " |")
-                           (first frame)
-                           (second frame)))))))
+            (princ
+             (format ";; [%3s] %s--> (%s %s ...)\n"
+                     frame-num
+                     (cond
+                      (kind "*")
+                      ((and (< (1+ fid) (length bt))
+                            (eql 't (first (second (elt bt (1+ fid))))))
+                       " /")
+                      (t " |"))
+                     (first frame)
+                     (second frame))))))
+      (princ ";; -------------------\n"))
 
-    :fixme))
+    ;; Ok, now to do real work:
 
+    ;; 0. Starting from the innermost frame, find the frame which is evald and
+    ;; has the name of the function in question.
+    (setq target-frame-num
+          (bsm-find-frame-num
+           (lambda (evald func args flags)
+             (and (eql 't evald)
+                  (eql caller-of func)))
+           bt
+           0))
+
+    ;; 1. Now, find the next outer bsm- frame. That's the answer of who called
+    ;; the caller-of.
+    (setq target-frame-num
+          (bsm-find-frame-num
+           (lambda (evald func args flags)
+             (and (eql 't evald)
+                  (string-prefix-p "bsm-" (symbol-name func))))
+           bt
+           (1+ target-frame-num)))
+
+    ;; 2. starting at this frame, collect all of the bsm- function going back
+    ;; to the outermost frame.
+    (setq bsm-func-stack
+          (mapcar (lambda (frame-spec) (second (second frame-spec)))
+                  (bsm-collect-frames
+                   (lambda (evald func args flags)
+                     (and (eql 't evald)
+                          (string-prefix-p "bsm-" (symbol-name func))))
+                   bt
+                   target-frame-num)))
+
+    ;; The first one is the caller of the caller-of frame. Then, ir continues
+    ;; to the most outer frame, but only of bsm- prefixed functions.
+    bsm-func-stack))
+
+;; used with (and it actually is) bsm-error
 (defun bsm-error (fmt &rest args)
   "Call error with the fmt and args, but try to find the most recently
 enclosing function prefixed with bsm- and put it into the error message."
-  (apply 'error (concat "Error: (%s): " fmt)
-         (bsm-get-bsm-caller-of 'bsm-error) args))
+  (apply 'error (concat "BSM Error: Stack: %s, Message: " fmt)
+         ;; Emit a nice list with arrows in it to help understand what it is.
+         (cl-loop for func on (bsm-get-bsm-caller-of 'bsm-error)
+                  append (if (cdr func)
+                             (list (car func) '<-)
+                           (list (car func))))
+         args))
+
+;; Testing functions for bsm-error
+
+(defun bsm-broken-garbage-0 ()
+  (bsm-euclidean-distance [0 0] [1 1 1]))
+(defun bsm-broken-garbage-1 ()
+  (bsm-broken-garbage-0))
+(defun bsm-broken-garbage-2 ()
+  (bsm-broken-garbage-1))
+(defun bsm-broken-garbage-3 ()
+  (bsm-broken-garbage-2))
+
+;;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Utilities
+;;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defun bsm-euclidean-distance (p0 p1)
   "Return the Euclidean distance between two points p0 and p1. The
@@ -90,19 +159,6 @@ points must be vectors and of the same length."
   (if (/= (length p0) (length p1))
       (bsm-error "Dimension mismatch: p0: %s p1: %s" p0 p1))
   (sqrt (reduce '+ (map 'vector '- p1 p0))))
-
-
-
-;; Testing functions for bsm-error
-
-(defun bsm-broken-garbage-0 ()
-  (bsm-error "Busted: %s" 42))
-(defun bsm-broken-garbage-1 ()
-  (bsm-broken-garbage-0))
-(defun bsm-broken-garbage-2 ()
-  (bsm-broken-garbage-1))
-(defun bsm-broken-garbage-3 ()
-  (bsm-broken-garbage-2))
 
 ;;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Types
@@ -140,12 +196,12 @@ points must be vectors and of the same length."
          (rect-edge-length (ceiling (sqrt (length buffers)))))
     (cl-loop for i below (length buffers)
              for buf in buffers do
-      (let ((x (mod i rect-edge-length))
-            (y (/ i rect-edge-length)))
-        (puthash (vector x y)
-                 (bsm-entity-buffer :bsm-loc (vector x y)
-                                    :bsm-buffer buf)
-                 *bsm-buffer-space-spaces*)))
+             (let ((x (mod i rect-edge-length))
+                   (y (/ i rect-edge-length)))
+               (puthash (vector x y)
+                        (bsm-entity-buffer :bsm-loc (vector x y)
+                                           :bsm-buffer buf)
+                        *bsm-buffer-space-spaces*)))
     *bsm-buffer-space-spaces*))
 
 (defun bsmg-dump-buffer-space ()
